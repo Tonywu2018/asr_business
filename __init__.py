4bf984bac2cca0534890cc4488de71c49cd9ca00
wuwenxiao <wuwenxiao@inke.cn>
2024-08-20 16:09:30 +0800

    添加项目文件

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from ._version import __title__, __version__
+from asr import ASR
+
+__all__ = ["__version__",
+           "__title__",
+           "ASR"]
---
+++ _version.py
@@ -0,0 +1,3 @@
+
+__title__ = "asr_business"
+__version__ = "0.1"  # x-release-please-version
